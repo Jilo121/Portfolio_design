d87971998adbb2fcbdcf29db553579e554b8a1a8
Nosy Hery Hasina <87439933+Jilo121@users.noreply.github.com>
2024-01-26 18:31:58 +0300

    Page principale

---
+++ assets/js/app.js
@@ -0,0 +1,16 @@
+let skills = document.querySelector('.skills-content');
+
+let skilllist = {
+    'name':['Adobe Photoshop','Adobe Illustrator','Adobe Premier Pro','Adobe After Effect','Adobe XD','Autodesk 3DS Max','Cinema 4D','Photography'],
+    'progress':[90,80,50,70,50,60,80,90]
+}
+
+for (i=0;i<skilllist.name.length;i++) {
+    const classwidth = "S" + i
+    let titreskill = '<p class="skills-title">'+skilllist.name[i]+'</p>'
+    let progressskils = '<div class="skills-progress"><div class="skill-per">'+ skilllist.progress[i] + '%</div><div class="skill-bar '+classwidth+'"></div></div>'
+    let skillcontainer = '<div class="skills">' + titreskill + progressskils + '</div>'
+    skills.innerHTML += skillcontainer
+    let widthcl = document.querySelector("."+classwidth)
+    widthcl.style.width = skilllist.progress[i] + "%"
+}
